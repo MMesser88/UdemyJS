627540a4ae911fddeaa45708cb50ef01e1e2eeb4
Anton <tihonov.as120188@gmail.com>
2024-10-17 21:26:40 +0300

    practice part one corrected

--- udemy/JS/script.js
+++ udemy/JS/script.js
@@ -1,49 +1,23 @@
-// let number = 5;
-// const leftBorderWidth = 1;
 
-// number = 10;
-// console.log(number);
-
-// const result = alert('Hello, World!');
-// console.log(HI);
-
-
-// const result = confirm('Ты здесь?');
-// console.log(result);
-
-let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
-console.log("Я уже посмотрел ",numberOfFilms," фильмов");
-
-let movies = {};
-let movieRank,
-    movieTitleOne,
-    movieTitleTwo;
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 let personalMovieDB = {
-    count: numberOfFilms,
-    movies: {
-        // movieTitleOne: movieRank,
-        // movieTitleTwo: movieRank,
-    },
-    actors: {},
-    genres: [],
-    privat: false
-}
+        count: numberOfFilms,
+        movies: {},
+        actors: {},
+        genres: [],
+        privat: false
+    };
 
-movieTitleOne = prompt('Один из последних просмотренных фильмов?', '');
-movieRank = prompt('На сколько вы его оцените?', '');
-movies[movieTitleOne] = movieRank;
-
-movieTitleTwo = prompt('Один из последних просмотренных фильмов?', '');
-movieRank = prompt('На сколько вы его оцените?', '');
-movies[movieTitleTwo] = movieRank;
-
-console.log(movies);
+let movieTitleOne = prompt('Один из последних просмотренных фильмов?', ''),
+    movieRankOne = prompt('На сколько вы его оцените?', ''),
+    movieTitleTwo = prompt('Один из последних просмотренных фильмов?', ''),
+    movieRankTwo = prompt('На сколько вы его оцените?', '');
 
 
 
-// console.log(answers);
-// document.write(answers);
+personalMovieDB.movies[movieTitleOne] = movieRankOne;
+personalMovieDB.movies[movieTitleTwo] = movieRankTwo;
 
-// const category = 'toys';
-// console.log(`https://someurl.com/${category}/5`)
+
+console.log(personalMovieDB);
