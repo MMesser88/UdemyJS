8c5b48938e9ed1c45b445b498deba151677672a8
Anton <tihonov.as120188@gmail.com>
2024-10-17 21:07:55 +0300

    practice part 1

--- udemy/JS/script.js
+++ udemy/JS/script.js
@@ -11,16 +11,39 @@
 // const result = confirm('Ты здесь?');
 // console.log(result);
 
-// const answer = prompt('How old are you?', '');
-// console.log(answer);
+let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+console.log("Я уже посмотрел ",numberOfFilms," фильмов");
 
-// const answers = [];
-// answers[0] = prompt('Как тебя зовут?', '');
-// answers[1] = prompt('Кто ты?', '');
-// answers[2] = prompt('Кто она?', '');
+let movies = {};
+let movieRank,
+    movieTitleOne,
+    movieTitleTwo;
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {
+        // movieTitleOne: movieRank,
+        // movieTitleTwo: movieRank,
+    },
+    actors: {},
+    genres: [],
+    privat: false
+}
+
+movieTitleOne = prompt('Один из последних просмотренных фильмов?', '');
+movieRank = prompt('На сколько вы его оцените?', '');
+movies[movieTitleOne] = movieRank;
+
+movieTitleTwo = prompt('Один из последних просмотренных фильмов?', '');
+movieRank = prompt('На сколько вы его оцените?', '');
+movies[movieTitleTwo] = movieRank;
+
+console.log(movies);
+
+
 
 // console.log(answers);
 // document.write(answers);
 
-const category = 'toys';
-console.log(`https://someurl.com/${category}/5`)
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/5`)
