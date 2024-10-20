3d4f4cb7cfee56c4e0c5e38c029307b295045af9
Anton <tihonov.as120188@gmail.com>
2024-10-20 08:12:47 +0300

    `buildigure`

--- udemy/JS/script.js
+++ udemy/JS/script.js
@@ -1,23 +1,26 @@
+// let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// let personalMovieDB = {
+//         count: numberOfFilms,
+//         movies: {},
+//         actors: {},
+//         genres: [],
+//         privat: false
+//     };
 
-let personalMovieDB = {
-        count: numberOfFilms,
-        movies: {},
-        actors: {},
-        genres: [],
-        privat: false
-    };
+// let movieTitleOne = prompt('Один из последних просмотренных фильмов?', ''),
+//     movieRankOne = prompt('На сколько вы его оцените?', ''),
+//     movieTitleTwo = prompt('Один из последних просмотренных фильмов?', ''),
+//     movieRankTwo = prompt('На сколько вы его оцените?', '');
 
-let movieTitleOne = prompt('Один из последних просмотренных фильмов?', ''),
-    movieRankOne = prompt('На сколько вы его оцените?', ''),
-    movieTitleTwo = prompt('Один из последних просмотренных фильмов?', ''),
-    movieRankTwo = prompt('На сколько вы его оцените?', '');
+// personalMovieDB.movies[movieTitleOne] = movieRankOne;
+// personalMovieDB.movies[movieTitleTwo] = movieRankTwo;
 
+// console.log(personalMovieDB);
 
-
-personalMovieDB.movies[movieTitleOne] = movieRankOne;
-personalMovieDB.movies[movieTitleTwo] = movieRankTwo;
-
-
-console.log(personalMovieDB);
+for (let i=1; i<9; i++) {
+    console.log(`*`);
+    for (let j=1; j>9; j++) {
+        console.log(`*`);
+    }
+}
